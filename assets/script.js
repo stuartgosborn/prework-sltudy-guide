5f78bbcf622aadb160dec70823dc040dc4da30a9
rauttornboss <stuartgosborn22@gmail.com>
2023-11-13 00:07:41 -0600

    added JavaScript functions to choose random topic to study and added web API's as a topic.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,30 @@
-var topics = ["HTML", "CSS", "Git", "JavaScript"];
-console.log(topics)
 
+var topics = ["HTML", "CSS", "Git", "JavaScript", "Web API's"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < topics.length; i++) {
+function listTopics() {
+    for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+ }
+};
 
-    console.log(`Let's study ${topics[i]}`)
- 
-
-}
-
-/* if (topic === 'HTML') {
+function selectTopic(){
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
+} else if (randomTopic === "Web API's") {
+    console.log("Let's study Web API's!");
 } else {
   console.log('Please try again!');
-}/*
+} 
+};
+
+console.log(`Here are the opics we learned through Prework:`);
+listTopics();
+console.log(`Which topic should we study first?`)
+selectTopic();
